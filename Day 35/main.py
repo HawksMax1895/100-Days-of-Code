5da48bdbdb7a5b7f57e9bbd0ee8a2f28a19462ff
maxra <maxradmacher@gmail.com>
2025-03-24 20:21:05 +0100

    Day 35 - final
    # Conflicts:
    #	Day 35/main.py

--- Day 35/main.py
+++ Day 35/main.py
@@ -1,17 +1,12 @@
 import requests
 import os
 from twilio.rest import Client
-from dotenv import load_dotenv
 
-load_dotenv()
-
-api_key = os.environ.get("OWM_API_KEY")
-account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
-auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
-print(api_key)
-
+api_key = "MY API KEY"
 lat = 50.76693006338142
 lon = 6.094452156110828
+account_sid = "ACCOUNT SID"
+auth_token = "AUTH TOKEN"
 
 weather = requests.get("https://api.openweathermap.org/data/2.5/forecast", params={'lat': lat, 'lon': lon, 'cnt': 4, 'appid': api_key})
 weather.raise_for_status()
@@ -21,12 +16,13 @@ forecast_weather = []
 
 for entry in forecast:
     if entry['weather'][0]['id'] < 600:
+        #print("Rain")
         client = Client(account_sid, auth_token)
 
         message = client.messages.create(
             body="It's going to rain today. Remember to bring an umbrella today",
-            from_="+12315946472",
-            to="+4917645999428",
+            from_="+MY TWILIO PHONE NUMBER",
+            to="+MY PHONE NUMBER",
         )
 
         print(message.body)
