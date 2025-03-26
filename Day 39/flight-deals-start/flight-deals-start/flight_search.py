37f69e4b06f01c1843b45a44c7ec4c0a2ef06315
maxra <maxradmacher@gmail.com>
2025-03-26 17:29:24 +0100

    Day 39 - final

--- Day 39/flight-deals-start/flight-deals-start/flight_search.py
+++ Day 39/flight-deals-start/flight-deals-start/flight_search.py
@@ -1,3 +1,73 @@
+import os
+import dotenv
+import requests
+
+dotenv.load_dotenv()
+
+IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        self._api_key = os.environ.get("AMADEUS_API_KEY")
+        self._api_secret = os.environ.get("AMADEUS_API_SECRET")
+        self._token = self._get_new_token()
+
+    def get_destination_codes(self, city_name):
+
+        headers = {
+            "Authorization": f"Bearer {self._token}"
+        }
+        query = {
+            "keyword": city_name,
+            "max": "2",
+            "include": "AIRPORTS",
+        }
+        response = requests.get(url=IATA_ENDPOINT, headers=headers, params=query)
+
+        try:
+            code = response.json()["data"][0]["iataCode"]
+        except IndexError:
+            print(f"IndexError: No airport code found for {city_name}")
+            return "N/A"
+        except KeyError:
+            print(f"KeyError: No airport found for {city_name}")
+            return "Not Found"
+        return code
+
+    def _get_new_token(self):
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        params = {
+            "grant_type": "client_credentials",
+            "client_id": self._api_key,
+            "client_secret": self._api_secret,
+        }
+        response = requests.post(url=TOKEN_ENDPOINT, headers=headers, data=params)
+        return response.json()["access_token"]
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {
+            "Authorization": f"Bearer {self._token}"
+        }
+        query = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": from_time.strftime("%Y-%m-%d"),
+            "returnDate": to_time.strftime("%Y-%m-%d"),
+            "adults": 1,
+            "nonStop": "true",
+            "currencyCode": "EUR",
+            "max": 10,
+        }
+        response = requests.get(url=FLIGHT_ENDPOINT, headers=headers, params=query)
+
+        if response.status_code != 200:
+            print(f"check_flight() response code: {response.status_code}")
+            print("Response body:", response.text)
+            return None
+
+        return response.json()
