f60612126ec97243eb65122e483f676ba6c87bfd
maxra <maxradmacher@gmail.com>
2025-03-26 10:32:28 +0100

    Day 37 - final

--- Day 37/main.py
+++ Day 37/main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import dotenv
+from datetime import datetime
 
 dotenv.load_dotenv()
 
@@ -30,4 +31,37 @@ graph_params = {
     "color": "sora",
 }
 
-requests.post(url=graph_endpoint, json=graph_params)
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.put(url=graph_endpoint, json=graph_params, headers=headers)
+# print(response.text)
+
+pixel_endpoint = f"{graph_endpoint}/graph1"
+
+today = datetime.today().strftime("%Y%m%d")
+print(today)
+
+pixel_params = {
+    "date": today,
+    "quantity": "10",
+}
+# response = requests.post(pixel_endpoint, json=pixel_params, headers=headers)
+# print(response.text)
+
+graph_params_update = {
+    "unit": "min",
+    "color": "momiji",
+}
+
+# response = requests.put(pixel_endpoint, json=graph_params_update, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixel_endpoint}/{today}"
+
+response = requests.delete(delete_endpoint, headers=headers)
+print(response.text)
+
+
+
