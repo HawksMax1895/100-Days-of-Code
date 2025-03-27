3460d1dcbf201b0c0861224ff1564a465a9a293e
maxra <maxradmacher@gmail.com>
2025-03-27 10:25:33 +0100

    Day 40 - start

--- Day 40/Day+40+Flight+Search+Amadeus+Start/main.py
+++ Day 40/Day+40+Flight+Search+Amadeus+Start/main.py
@@ -47,16 +47,11 @@ for destination in sheet_data:
 
     if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
         print(f"Lower price flight found to {destination['city']}!")
-        # notification_manager.send_sms(
-        #     message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
-        #                  f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
-        #                  f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
-        # )
-        # SMS not working? Try whatsapp instead.
-        notification_manager.send_whatsapp(
+        notification_manager.send_sms(
             message_body=f"Low price alert! Only £{cheapest_flight.price} to fly "
                          f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
                          f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
         )
+
 
 
