d676f2fd64894bb132f81ac9e3af01102b2e3ee1
maxra <maxradmacher@gmail.com>
2025-04-01 20:14:54 +0200

    Day 50 - start

---
+++ Day 50/main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+URL = "https://tinder.com/"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(URL)
+
+
+
+
+
